493a1f14e0f93fe5658d236a80c6ab006f799252
VT-Zhang <jianz@vt.edu>
2017-06-19 15:14:02 -0400

    Added some practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Aperiomics Dashboard';
+  title: string = 'Aperiomics Dashboard';
+  number = 10;
+  name = 'Jason Zhang';
+  user = {
+    first_name: 'Bill',
+    last_name: 'Gates',
+  }
+  x: number = 7;
+  num: number = 1;
+  today = new Date();
+  arr = [1,2,3,4,5];
+  boo = true;
+
+  logNum(){
+    this.num += 1;
+    console.log('num is: ', this.num);
+  }
+
+  users = [];
+  user1 = {
+    email: ''
+  };
+  onSubmit(){
+    this.users.push(this.user2);
+    this.user2 = new User();
+  }
+
+  user2 = new User();
+
+  
 }
