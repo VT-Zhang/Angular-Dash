c30acdba1f4dd284d3f20e281b430cd0e4f854f3
VT-Zhang <jianz@vt.edu>
2017-06-19 16:01:56 -0400

    Practice with registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,33 +9,13 @@ import { User } from './user';
 
 export class AppComponent {
   title: string = 'Aperiomics Dashboard';
-  number = 10;
-  name = 'Jason Zhang';
-  user = {
-    first_name: 'Bill',
-    last_name: 'Gates',
-  }
-  x: number = 7;
-  num: number = 1;
-  today = new Date();
-  arr = [1,2,3,4,5];
-  boo = true;
-
-  logNum(){
-    this.num += 1;
-    console.log('num is: ', this.num);
-  }
-
   users = [];
-  user1 = {
-    email: ''
-  };
-  onSubmit(){
-    this.users.push(this.user2);
-    this.user2 = new User();
+  user = new User();
+  submitted: boolean = false;
+  register(){
+    this.users.push(this.user);
+    console.log(this.users);
+    this.user = new User();
+    this.submitted = true;
   }
-
-  user2 = new User();
-
-  
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -5,6 +5,10 @@ export class User {
         public last_name: string = '',
         public email: string = '',
         public password: string = '',
+        public street: string = '',
+        public city: string = '',
+        public state: string = '',
+        public lucky: boolean = true,
         public created_at: Date = new Date(),
         public updated_at: Date = new Date()
     ){}
