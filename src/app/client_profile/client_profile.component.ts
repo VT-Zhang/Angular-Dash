b6f4ce77fbf2a6f73826b7c2514f596ec0e14fff
VT-Zhang <jianz@vt.edu>
2017-06-20 15:00:34 -0400

    fake test json data for client profile component

--- src/app/client_profile/client_profile.component.ts
+++ src/app/client_profile/client_profile.component.ts
@@ -12,4 +12,31 @@ export class ClientProfileComponent implements OnInit {
   ngOnInit() {
   }
 
+  clients = [
+    {
+      'id': '1',
+      'type': 'Clinic',
+      'name': 'Inova Hospital',
+      'phone': '703-288-9953',
+      'fax': '703-288-9955',
+      'email': 'info@inova.org',
+      'street': '123 Main Street',
+      'city': 'Sterling',
+      'state': 'VA',
+      'zipcode': '20165',
+    },
+    {
+      'id': '2',
+      'type': 'Hospital',
+      'name': 'GW Hospital',
+      'phone': '703-288-9953',
+      'fax': '703-288-9955',
+      'email': 'info@gwu.edu',      
+      'street': '456 Second Street',
+      'city': 'Ashburn',
+      'state': 'VA',
+      'zipcode': '20170',
+    }
+  ]
+
 }
